3165e9dbcbb518845733c851138961b2c2d2209e
Kadie Jenkins <179417967+kdelise@users.noreply.github.com>
2025-12-21 16:03:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
       8: "Curve ball but it's a really cute story I promise<br><a href='https://archiveofourown.org/works/62988229' target='_blank'>Minho x Kitty</a>",
       9: "!NSFW! <br> shitpost <a href='https://archiveofourown.org/works/71290671' target='_blank'>ChenJi</a>",
       10: "Ryo presents and Sakuya is there to comfort him<br><a href='https://archiveofourown.org/works/68548421' target='_blank'>SakuRyo</a>",
-      11: "Story for Day 11!",
+      11: "I might've already sent this one<br>but enjoy trans<a href='https://archiveofourown.org/works/56401864' target='_blank'>TtoBin</a>",
       12: "Story for Day 12!",
       13: "<a href='https://archiveofourown.org/works/29008359/chapters/71195499' target='_blank'>KunTen</a><br>Single dad Kun with his triplets who make Ten fall even harder",
       14: "Story for Day 14!",
